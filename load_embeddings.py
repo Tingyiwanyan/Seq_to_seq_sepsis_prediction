2a83771a5e714abd74fbfdc1b3c81ab0d7ca0eff
Tingyi Wanyan <tiwanyan@gmail.com>
2022-06-01 16:35:18 -0400

    add load embedding

---
+++ load_embeddings.py
@@ -0,0 +1,37 @@
+from sklearn.metrics import roc_auc_score
+from sklearn.manifold import TSNE
+import matplotlib.pyplot as plt
+import numpy as np
+import random
+
+
+def load_embedding(self):
+    with open('on_site_embedding.npy', 'rb') as f:
+        self.on_site_embedding = np.load(f)
+    with open('on_site_logit.npy', 'rb') as f:
+        self.on_site_logit = np.load(f)
+    with open('temporal_semantic_embedding.npy', 'rb') as f:
+        self.temporal_semantic_embedding = np.load(f)
+
+
+def vis_embedding_load(self):
+    CL_k = TSNE(n_components=2).fit_transform(np.array(self.on_site_embedding)[0:5000, :])
+    for i in range(5000):
+        if self.on_site_logit[i] == 0:
+            plt.plot(CL_k[i][0], CL_k[i][1], 'o', color='blue', markersize=1)
+        if self.on_site_logit[i] == 1:
+            plt.plot(CL_k[i][0], CL_k[i][1], 'o', color='red', markersize=5)
+
+    plt.show()
+
+
+def vis_embedding_tsl_load(self):
+    CL_k = TSNE(n_components=2).fit_transform(self.temporal_semantic_embedding[0:5000, :])
+
+    for i in range(5000):
+        if self.train_logit[i] == 0:
+            plt.plot(CL_k[i][0], CL_k[i][1], 'o', color='blue', markersize=1)
+        if self.train_logit[i] == 1:
+            plt.plot(CL_k[i][0], CL_k[i][1], 'o', color='red', markersize=5)
+
+    plt.show()
