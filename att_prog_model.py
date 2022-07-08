42afec18e051af1cfb1db0f37644e33c5c421979
Tingyi Wanyan <tiwanyan@gmail.com>
2022-07-08 16:05:39 -0400

    add new method

---
+++ att_prog_model.py
@@ -0,0 +1,199 @@
+from tensorflow.keras import layers
+import tensorflow as tf
+from tensorflow import keras
+from scipy.stats import ortho_group
+from sklearn.metrics import roc_auc_score
+from sklearn.manifold import TSNE
+import matplotlib.pyplot as plt
+import numpy as np
+import random
+import pandas as pd
+
+semantic_step_global = 6
+semantic_positive_sample = 4
+unsupervised_cluster_num = 10
+latent_dim_global = 100
+positive_sample_size = 10
+batch_size = 128
+unsupervised_neg_size = 5
+reconstruct_resolution = 7
+
+
+class projection(keras.layers.Layer):
+    def __init__(self, units=unsupervised_cluster_num, input_dim=latent_dim_global):
+        super(projection, self).__init__()
+        w_init = tf.random_normal_initializer()
+        #w_init = tf.keras.initializers.Orthogonal()
+        self.w = tf.Variable(
+            initial_value=w_init(shape=(units, input_dim), dtype="float32"),
+            trainable=True,
+        )
+        # b_init = tf.zeros_initializer()
+        # self.b = tf.Variable(
+        # initial_value=b_init(shape=(units,), dtype="float32"), trainable=True
+        # )
+
+    def call(self, inputs):
+        return tf.math.multiply(inputs, self.w)
+
+
+class protatype_ehr():
+    def __init__(self, projection):
+        #self.read_d = read_d
+        self.projection_model = projection
+        #self.train_data = read_d.train_data
+        #self.test_data = read_d.test_data
+        #self.validate_data = read_d.val_data
+        #self.length_train = len(self.train_data)
+        #self.length_test = len(self.test_data)
+        #self.length_val = len(self.validate_data)
+        self.loss_tracker = tf.keras.metrics.Mean(name="loss")
+
+        self.ave_all = [8.38230435e+01, 9.75000000e+01, 3.69060000e+01, 1.18333333e+02,
+                        7.71140148e+01, 5.90000000e+01, 1.81162791e+01, 0.00000000e+00,
+                        -2.50000000e-01, 2.43333333e+01, 5.04195804e-01, 7.38666667e+00,
+                        4.00504808e+01, 9.60000000e+01, 4.20000000e+01, 1.65000000e+01,
+                        7.70000000e+01, 8.35000000e+00, 1.06000000e+02, 9.00000000e-01,
+                        1.16250000e+00, 1.25333333e+02, 1.65000000e+00, 2.00000000e+00,
+                        3.36666667e+00, 4.08000000e+00, 7.00000000e-01, 3.85000000e+00,
+                        3.09000000e+01, 1.05000000e+01, 3.11000000e+01, 1.08333333e+01,
+                        2.55875000e+02, 1.93708333e+02]
+
+        self.std_all = [1.40828962e+01, 2.16625304e+00, 5.53108392e-01, 1.66121889e+01,
+                        1.08476132e+01, 9.94962122e+00, 3.59186362e+00, 0.00000000e+00,
+                        3.89407506e+00, 3.91858658e+00, 2.04595954e-01, 5.93467422e-02,
+                        7.72257867e+00, 8.87388075e+00, 5.77276895e+02, 1.79879091e+01,
+                        1.36508822e+02, 6.95188900e-01, 5.09788015e+00, 1.43347221e+00,
+                        3.75415153e+00, 4.03968485e+01, 1.71418146e+00, 3.15505742e-01,
+                        1.17084555e+00, 4.77914796e-01, 3.62933460e+00, 9.91058703e+00,
+                        4.60374699e+00, 1.64019340e+00, 1.68795640e+01, 6.23941196e+00,
+                        1.75014175e+02, 1.03316340e+02]
+
+        """
+        define hyper-parameters
+        """
+        self.gaussian_mu = 0
+        self.gaussian_sigma = 0.0001
+        self.batch_size = batch_size
+        self.neg_size = self.batch_size
+        self.pos_size = positive_sample_size
+        self.reconstruct_resolution = reconstruct_resolution
+        self.vital_length = 8
+        self.lab_length = 19
+        self.blood_length = 27
+        self.epoch = 20
+        self.feature_num = 34
+        self.pre_train_epoch = 7
+        self.latent_dim = latent_dim_global
+        self.tau = 1
+        self.time_sequence = 48#self.read_d.time_sequence
+        self.tcn_filter_size = 3
+        self.semantic_time_step = semantic_step_global
+        self.unsupervised_cluster_num = unsupervised_cluster_num
+        self.unsupervised_neg_size = unsupervised_neg_size
+        self.start_sampling_index = 5
+        self.sampling_interval = 5
+        self.converge_threshold_E = 200
+        self.semantic_positive_sample = semantic_positive_sample
+        self.max_value_projection = np.zeros((self.batch_size, self.semantic_time_step))
+        self.basis_input = np.ones((self.unsupervised_cluster_num, self.latent_dim))
+
+        self.create_memory_bank()
+        self.length_train = len(self.train_data)
+
+        """
+        initialize orthogonal projection basis
+        """
+        self.initializer_basis = tf.keras.initializers.Orthogonal()
+        self.init_projection_basis = tf.Variable(
+            self.initializer_basis(shape=(self.unsupervised_cluster_num, self.latent_dim)))
+
+        self.steps = self.length_train // self.batch_size
+        self.lr_decayed_fn = tf.keras.optimizers.schedules.CosineDecay(
+            initial_learning_rate=0.0003, decay_steps=self.steps)
+
+        """
+        initialize position encoding vectors
+        """
+        self.position_embedding = np.zeros((self.semantic_time_step, self.latent_dim))
+        self.generate_orthogonal = ortho_group.rvs(self.latent_dim)
+        for i in range(self.semantic_time_step):
+            # self.position_embedding[i,:] = self.position_encoding(i)
+            self.position_embedding[i, :] = self.generate_orthogonal[i]
+
+        # self.batch_position_embedding = np.expand_dims(self.position_embedding,0)
+        # self.batch_position_embedding = np.broadcast_to(self.batch_position_embedding,[self.batch_size,
+        # self.semantic_time_step,
+        # self.latent_dim])
+
+        self.lr_schedule = tf.keras.optimizers.schedules.ExponentialDecay(
+            initial_learning_rate=0.0003,
+            decay_steps=self.steps,
+            decay_rate=0.3)
+
+    def create_memory_bank(self):
+        # self.train_data, self.train_logit,self.train_sofa,self.train_sofa_score = self.aquire_data(0, self.train_data, self.length_train)
+        # self.test_data, self.test_logit,self.test_sofa,self.test_sofa_score = self.aquire_data(0, self.test_data, self.length_test)
+        # self.val_data, self.val_logit,self.val_sofa,self.val_sofa_score = self.aquire_data(0, self.validate_data, self.length_val)
+
+        #file_path = '/home/tingyi/physionet_data/Interpolate_data/'
+        file_path = '/prj0129/tiw4003/Interpolate_data/'
+        with open(file_path + 'train.npy', 'rb') as f:
+            self.train_data = np.load(f)
+        with open(file_path + 'train_logit.npy', 'rb') as f:
+            self.train_logit = np.load(f)
+        with open(file_path + 'train_on_site_time.npy', 'rb') as f:
+            self.train_on_site_time = np.load(f)
+
+        # with open(file_path + 'test.npy', 'rb') as f:
+        # self.test_data = np.load(f)
+        # with open(file_path + 'test_logit.npy', 'rb') as f:
+        # self.test_logit = np.load(f)
+        with open(file_path + 'val.npy', 'rb') as f:
+            self.val_data = np.load(f)
+        with open(file_path + 'val_logit.npy', 'rb') as f:
+            self.val_logit = np.load(f)
+        with open(file_path + 'val_on_site_time.npy', 'rb') as f:
+            self.val_on_site_time = np.load(f)
+        # with open(file_path + 'train_sofa_score.npy', 'rb') as f:
+        # self.train_sofa_score = np.load(f)
+        # with open(file_path + 'train_sofa.npy', 'rb') as f:
+        # self.train_sofa = np.load(f)
+
+        with open(file_path + 'train_origin.npy', 'rb') as f:
+            self.train_data_origin = np.load(f)
+
+        self.max_train_data = np.max(np.reshape(self.train_data,(self.train_data.shape[0]*self.train_data.shape[1],
+                                                                 self.train_data.shape[2])),0)
+        self.min_train_data = np.min(np.reshape(self.train_data,(self.train_data.shape[0]*self.train_data.shape[1],
+                                                                 self.train_data.shape[2])),0)
+
+        self.index_train = np.array(range(self.train_data.shape[0]))
+
+        self.train_data_range = self.max_train_data - self.min_train_data
+        for i in range(self.train_data_range.shape[0]):
+            if self.train_data_range[i] == 0:
+                self.train_data_range[i] = 1
+
+        self.train_data_norm = (np.reshape(self.train_data,
+                                           (self.train_data.shape[0]*self.train_data.shape[1],
+                                                                 self.train_data.shape[2])) - self.min_train_data)\
+                               /self.train_data_range
+
+        self.train_data_norm = np.reshape(self.train_data_norm,(self.train_data.shape[0],self.train_data.shape[1],
+                                                                self.train_data.shape[2]))
+
+        self.train_dataset = tf.data.Dataset.from_tensor_slices(
+            (self.train_data_norm, self.train_logit, self.train_on_site_time, self.train_data_norm,self.index_train))  # ,self.train_sofa_score))
+        self.train_dataset = self.train_dataset.shuffle(buffer_size=1024).batch(self.batch_size)
+        cohort_index = np.where(self.train_logit == 1)[0]
+        control_index = np.where(self.train_logit == 0)[0]
+        self.memory_bank_cohort = self.train_data[cohort_index, :, :]
+        self.memory_bank_control = self.train_data[control_index, :, :]
+        self.memory_bank_cohort_on_site = self.train_on_site_time[cohort_index]
+        self.memory_bank_control_on_site = self.train_on_site_time[control_index]
+        self.memory_bank_cohort_origin = self.train_data_origin[cohort_index]
+        self.memory_bank_control_origin = self.train_data_origin[control_index]
+        self.num_cohort = self.memory_bank_cohort.shape[0]
+        self.num_control = self.memory_bank_control.shape[0]
+
