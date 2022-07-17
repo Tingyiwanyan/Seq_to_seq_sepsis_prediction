4872a84016a8f8ae102587509d0eed33d2a62c6a
Tingyi Wanyan <tiwanyan@gmail.com>
2022-07-17 19:24:07 -0400

    correction

--- cvae.py
+++ cvae.py
@@ -79,11 +79,14 @@ class att_temporal(keras.layers.Layer):
         super(att_temporal, self).__init__(**kwargs)
 
     def build(self, input_shape):
-        self.kernel = self.add_weight(name = 'kernel', shape = (input_shape[-1], self.output_dim),
+        self.kernel_key = self.add_weight(name = 'kernel_key', shape = (input_shape[-1], self.output_dim),
                                       initializer = tf.keras.initializers.he_normal(seed=None), trainable = True)
 
         self.kernel_quary = self.add_weight(name = 'kernel_quary', shape = (input_shape[-1], self.output_dim),
                                       initializer = tf.keras.initializers.he_normal(seed=None), trainable = True)
+
+        self.kernel_value = self.add_weight(name='kernel_value', shape=(input_shape[-1], self.output_dim),
+                                            initializer=tf.keras.initializers.he_normal(seed=None), trainable=True)
 
     def call(self, input_data):
         conv_layers_outputs = []
@@ -99,15 +102,16 @@ class att_temporal(keras.layers.Layer):
 
         self.check_input_previous = input_previous
         self.check_input_after = input_after
-        att_output = tf.matmul(input_after,self.kernel)
-        att_output_quary = tf.matmul(input_previous,self.kernel_quary)
+        att_output = tf.matmul(input_after,self.kernel_quary)
+        att_output_key = tf.matmul(input_previous,self.kernel_key)
         #att_output = tf.math.exp(tf.matmul(att_output,tf.transpose(att_output_quary,perm=[0,1,3,2])))
-        att_output = tf.matmul(att_output, tf.transpose(att_output_quary, perm=[0, 1, 3, 2]))/10
+        att_output = tf.matmul(att_output, tf.transpose(att_output_key, perm=[0, 1, 3, 2]))/10
         att_output = tf.keras.activations.softmax(att_output, axis=-1)
 
         att_output = tf.expand_dims(att_output,axis=-1)
         self.check_att_output = att_output
-        input_data_compare = tf.expand_dims(input_data,axis=2)[:,0:-1,:,:,:]
+        input_data_compare = tf.matmul(input_data,self.kernel_value)
+        input_data_compare = tf.expand_dims(input_data_compare,axis=2)[:,0:-1,:,:,:]
         self.check_input_data_compare = input_data_compare
         progression_embedding = tf.reduce_sum(tf.math.multiply(input_data_compare,att_output),axis=-2)
         self.check_progression_embedding = progression_embedding
